60a915b0d3ee7ee6a8eab3c7368e4abe3a2e08d9
shunya kataoka <shunya_kataoka@fringe81.com>
2020-05-16 00:31:09 +0900

    ページ送りが「前へ」onlyの場合も戻れるように

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 const prev = document.querySelector(".paging-lay .blk1-lay a");
 const next = document.querySelector(".paging-lay .blk2-lay a");
+const prevOnly = document.querySelector(".paging-lay .blk3-lay a");
 
 document.addEventListener("keydown", event => {
     switch(event.keyCode) {
         case 37:
-            prev.click();
+            prev ? prev.click() : prevOnly.click();
             break;
         case 39:
             next.click();
